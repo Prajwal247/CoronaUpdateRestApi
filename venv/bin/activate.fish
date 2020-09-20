e111722c81b39b55b5c46a63ce69236dd3f6a232
xkshitizx <kshitizlama03@gmail.com>
2020-09-20 20:40:03 +0545

    Added requirements.txt

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/prazzwal/Desktop/Coronatimes/coronatimes/venv"
+set -gx VIRTUAL_ENV "/home/kshitiz/CoronaUpdateRestApi/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
