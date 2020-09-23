b94983ef0d4a275a53a1ae87d9313743abf2c846
xkshitizx <kshitizlama03@gmail.com>
2020-09-23 14:45:05 +0545

    Procfile edit.

--- coronatimes/settings.py
+++ coronatimes/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,4 +140,3 @@ SUMMERNOTE_CONFIG = {
     'width':'98%',
     }
 }
-django_heroku.settings(locals())
